a43a76949c2ca183b1fd226f71c6318cabd6c97f
Kevin Maurer <kevinmaurer2011@gmail.com>
2018-07-30 11:13:31 -0500

    ...

--- Week 3/testing.py
+++ Week 3/testing.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 photo_data = misc.imread('C:/Users/kevin/Documents/PyCharm/python-for-data-science/Week 3/sd-3layers.jpg')
 
-red_mask = photo_data[:, :, 0] < 0  # red
-green_mask = photo_data[:, :, 1] < 0  # green
-blue_mask = photo_data[:, :, 2] < 0  # blue
+red_mask = photo_data[:, :, 0] < 200  # red
+green_mask = photo_data[:, :, 1] < 200  # green
+blue_mask = photo_data[:, :, 2] < 200  # blue
 
 final_mask = np.logical_and(red_mask, green_mask, blue_mask)
 photo_data[final_mask] = 0
